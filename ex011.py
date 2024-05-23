edcab4002dc05ad014f0500ca902885e2a7fa183
Túlio Araujo <tulioharaujoo@gmail.com>
2024-05-23 18:02:29 -0300

    Treinando
    Continuei tentando resolver alguns exercícios

--- ex011.py
+++ ex011.py
@@ -1,6 +1,12 @@
 # Mostre a largura e altura de uma parede em metros
 # Calcule a area e a quantidade de tinta necessária para pintar sabendo que cada litro de tinta
 # pinta uma área de 2m²
+print('Olá, seja bem-vindo ao nosso site!')
+lag = float(input('Digite a largura da sua parede: '))
+alt = float(input('Digite a altura da sua parede: '))
+area = lag * alt
+tinta = area/2
+print('Para pintar o espaço informado medindo {0}m de altura por {1}m de largura, você irá precisar de {2}L de tinta.'.format(lag, alt, tinta))
 
 # Aplique desconto de 5% em um produto?
 
---
+++ ex012.py
@@ -0,0 +1,9 @@
+# Aplique desconto de 5% em um produto?
+print('Olá, seja bem-vindo ao nosso site!')
+prod = float(input('Digite o preço do produto: '))
+desc = (5/100) * prod
+final = desc * prod
+print(final)
+
+# MANO EU NÃO SEI FAZER REGRA DE TRÊS VSF
+# Leia o salário atual e mostre o aumento com 15% de aumento?
