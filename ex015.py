ba3be1e67c44ff9147985bd990a1606ed38ac19f
Túlio Araujo <tulioharaujoo@gmail.com>
2024-05-29 17:50:02 -0300

    Treinando
    Esses dois exercícios não estavam previstos na aula 07, porém consegui realiza-los. Acredito que fazer um pouquinho por dia tem facilitado a aprendizagem.

---
+++ ex015.py
@@ -0,0 +1,11 @@
+"""Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado e a quantidade
+de dias pelos quais ele foi alugado. Calcule o preço  a pagar, sabendo que o carro custa R$60 por dia
+e R$0,15 por km rodado.
+"""
+print('Olá, seja bem-vindo(a)!')
+dias = int(input('Por quantos dias o carro ficou alugado? '))
+dp = float(input('Quantos km foram percorridos? '))
+c = dias * 60  # eu fiz de uma forma mais complexa talvez kkkk
+km = (dp * 0.15) + c
+"""solução da aula | pago = (d * 60) + (dp * 0.15)"""
+print('Você alugou o carro por {} dias e percorreu {}km.\nO valor total é de R${:.2f}.'.format(dias, dp, km))
