fe8349133c05a92ebb357bfe5aaaf46dffd1debb
Túlio Araujo <tulioharaujoo@gmail.com>
2024-06-06 10:27:09 -0300

    exercicios
    Esses dois exercícios ainda não tinha comitado

--- ex014.py
+++ ex014.py
@@ -1,5 +1,5 @@
 # Escreva um programa que converta uma temperatura digitada em ºc e converta para ºF.
 """Sabendo que 0º celsius é o equivalente a 32ºF"""
 g = float(input('Digite a temperatura em ºC: '))
-#f = (g*9/5) + 32 nesse caso o parênteses não influência a ordem de precedência
+#f = (g*9/5) + 32 nesse caso o parênteses não influência a ordem de precedência.
 print('A temperatura de {}ºC é equivalente a {}ºF.'.format(g, g * 9 / 5 + 32))
--- ex015.py
+++ ex015.py
@@ -5,7 +5,7 @@ e R$0,15 por km rodado.
 print('Olá, seja bem-vindo(a)!')
 dias = int(input('Por quantos dias o carro ficou alugado? '))
 dp = float(input('Quantos km foram percorridos? '))
-c = dias * 60  # eu fiz de uma forma mais complexa talvez kkkk
+c = dias * 60  # eu fiz de uma forma mais complexa talvez kkk
 km = (dp * 0.15) + c
 """solução da aula | pago = (d * 60) + (dp * 0.15)"""
 print('Você alugou o carro por {} dias e percorreu {}km.\nO valor total é de R${:.2f}.'.format(dias, dp, km))
