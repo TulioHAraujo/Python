b964810647c095862a0ddf112c2794c956507ad2
Túlio Araujo <tulioharaujoo@gmail.com>
2024-06-12 22:09:39 -0300

    exercicios
    Aula 8 assistida, desafios propostos, agora vou estudar para poder resolve-los antes de assistir as aulas de exercícios corrigidos.

---
+++ ex016.py
@@ -0,0 +1,28 @@
+"""
+Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
+EX: Digite um número: 6.127.
+O número 6.127 tem a parte inteira 6.
+Dica: olhar a biblioteca math
+
+Ex017
+Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule
+e mostre o comprimento da hipotenusa.
+
+Ex018
+Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+
+Ex019
+Um professor quer sortearum dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o
+nome deles e escrevendo o nome do escolhido.
+import random.
+
+Ex020
+O professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que
+leia o nome dos quatro alunos e mostre a ordem sorteada.
+Esse é um pouquinho mais dificil.
+
+Ex021
+Faça um programa que abra e reproduza um áudio de um arquivo mp3.
+Difícil.
+"""
+
