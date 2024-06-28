2cf40d90c6f651c0581a2e20503e6a2d78c6b11d
Túlio Araujo <tulioharaujoo@gmail.com>
2024-06-27 22:34:40 -0300

    treinando
    Achei esse exercício difícil, pois a biblioteca pygame estava confusa de entender a sintaxe e desatualizada em relação a ter o input() antes do pygame.event.wait()

--- ex020.py
+++ ex020.py
@@ -3,10 +3,6 @@ Ex020
 O professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que
 leia o nome dos quatro alunos e mostre a ordem sorteada.
 Esse é um pouquinho mais dificil.
-
-Ex021
-Faça um programa que abra e reproduza um áudio de um arquivo mp3.
-Difícil.
 """
 
 from random import sample
--- ex021.py
+++ ex021.py
@@ -2,4 +2,16 @@
 Ex021
 Faça um programa que abra e reproduza um áudio de um arquivo mp3.
 Difícil.
+
+Esse exercício foi bem complexo no meu entendimento, a biblioteca não está tão atualizada
+e sua sintaxe é complexa, consegui resolver apenas assistindo à aula.
+Mas foi válido, porque fiz algumas pesquisas.
 """
+import pygame
+
+pygame.init()
+
+pygame.mixer.music.load('Vlad-Gluschenko-Summer-Goes.mp3')
+pygame.mixer.music.play()
+input() #input() antes da linha abaixo, por isso não estava tocando.
+pygame.event.wait()
