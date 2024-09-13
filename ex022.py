c5ea80d9c2036a890ece087114bf411516afbd98
Túlio Araujo <tulioharaujoo@gmail.com>
2024-09-12 22:40:06 -0300

    Criando exercícios para treinar depois

--- ex022.py
+++ ex022.py
@@ -2,6 +2,6 @@
 Crie um programa que leia o nome completo de uma pessoa e mostre:
 1- O nome com todas as letras maiúsculas.
 2- O nome com todas as letras minúsculas.
-3- Quantas letras ao todo(sem considerar espaços). #atenção retirar espaços internos
+3- Quantas letras ao todo (sem considerar espaços). #atenção retirar espaços internos
 4- Quantas letras tem o primeiro nome.
 '''
