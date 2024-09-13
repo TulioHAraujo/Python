d0faa8ab97ebb4cd797c5e7e4a73ddd752d87487
Túlio Araujo <tulioharaujoo@gmail.com>
2024-09-12 22:38:27 -0300

    Criando exercícios para treinar depois

---
+++ ex022.py
@@ -0,0 +1,7 @@
+'''
+Crie um programa que leia o nome completo de uma pessoa e mostre:
+1- O nome com todas as letras maiúsculas.
+2- O nome com todas as letras minúsculas.
+3- Quantas letras ao todo(sem considerar espaços). #atenção retirar espaços internos
+4- Quantas letras tem o primeiro nome.
+'''
---
+++ ex025.py
@@ -0,0 +1,5 @@
+'''
+Crie um programa que leia o nome de uma pessoa e diga
+se ela tem "SILVA" no nome.
+DICA - achar o true ou false
+'''
