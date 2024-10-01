fc5ba8546d7beae7dca9db5304ab37f82dff0322
Túlio Araujo <tulioharaujoo@gmail.com>
2024-09-30 21:58:04 -0300

    Treinando
    Exercício simples, porém o detalhe é o que faz a diferença

--- ex025.py
+++ ex025.py
@@ -2,4 +2,21 @@
 Crie um programa que leia o nome de uma pessoa e diga
 se ela tem "SILVA" no nome.
 DICA - achar o true ou false
+
+Esse programa diferente do anterior quer encontrar
+o nome silva independente da posição.
 '''
+
+'''Travei na parte mais simples, era só ter colocado o nome em upper ou lower
+e depois da variável nome colocar .upper() ou .lower().
+Lembrando que in é um operador.'''
+'''nome = str(input('Digite seu nome completo: ')).strip()
+busca = 'SILVA' in nome.upper()
+print(nome)
+print('Seu nome tem Silva!', busca)
+'''
+
+#Guanabara fez da seguinte forma
+
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Seu nome tem Silva ? {}'.format('silva' in nome.lower()))
